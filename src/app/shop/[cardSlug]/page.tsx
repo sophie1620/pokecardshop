2d03c6b7537a46d5e5d2a3504f7b89452b052b3c
sophie <sophie1620@gmail.com>
2024-12-09 15:00:58 -0500

    updated individual card page layout

--- src/app/shop/[cardSlug]/page.tsx
+++ src/app/shop/[cardSlug]/page.tsx
@@ -54,42 +54,56 @@ export default async function CardPage({params}: {params: Promise<{cardSlug: str
           ></Image>
         </div>
 
-        <div className="flex flex-row justify-end items-center gap-4">
-          <p>Cost: {cost}</p>
-          <AddToCartBtn item={card} />
-        </div>
+        <section className="md:w-3/5  flex flex-col items-start mx-auto">
 
-        { card.description && 
-          <p className="text-center mt-4">{card.description}</p>}
+          <div className="flex flex-row justify-end items-center gap-4 w-full">
+            <p>Cost: {cost}</p>
+            <AddToCartBtn item={card} />
+          </div>
 
-        <div className={classes.detail}>
-          <h3>Type(s):</h3>
-          {card.types.map((type: string) => (
-            <p key={type}> {type}</p>
-          ))}
-        </div>
+          {card.description && 
+            <div>
+              <h3>Description:</h3>
+              <p className="my-4"> {card.description}</p>
+            </div>
+            }
 
-        <div className={classes.detail}>
-          <h3>Attacks:</h3>
-            {card.attacks.map((attack: IAttack, i: number) => (
-              <div key={Math.random()}>
-                <p>Attack {i+1}: {attack.name}</p>
-                <p>Effect: {attack.effect}</p>
-                <p>Cost: {attack.cost.join(', ')}</p>
-                <p>Damage: {attack.damage}</p>
+          <div>
+            <h3>Type(s):</h3>
+            {card.types.map((type: string) => (
+              <p className={classes.cardDetail} key={type}> {type}</p>
+            ))}
+          </div>
+
+          <div>
+            <h3>Attacks:</h3>
+              {card.attacks.map((attack: IAttack, i: number) => (
+                <div className={classes.cardDetail} key={Math.random()}>
+                  <p>
+                    <span>Attack {i+1}:</span> {attack.name}
+                  </p>
+                  <p>
+                    <span>Cost:</span> {attack.cost.join(', ')}
+                  </p>
+                  <p>
+                    <span>Damage:</span> {attack.damage}
+                  </p>
+                </div>
+            ))}
+          </div>
+
+          <div>
+            <h3>Weaknesses:</h3>
+            {card.weaknesses.map((weak: IWeakness) => (
+              <div className={classes.cardDetail} key={Math.random()}>
+                <p>
+                  <span>Type:</span> {weak.type}</p>
+                <p>
+                  <span>Value:</span> {weak.value}</p>
               </div>
             ))}
-        </div>
-
-        <div className={classes.detail}>
-          <h3>Weaknesses:</h3>
-          {card.weaknesses.map((weak: IWeakness) => (
-            <div key={Math.random()}>
-              <p>Type: {weak.type}</p>
-              <p>Value: {weak.value}</p>
-            </div>
-          ))}
-        </div>
+          </div>
+        </section>
       </Suspense>
     </main>
   )
